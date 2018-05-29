ceef4b16c4b47306ae336a564415cd616b9860c5
evpobr <evpobr@gmail.com>
2018-05-29 20:07:37 +0500

    Add VS2017 project

---
+++ projects/VS2017/taglib/taglib/stdafx.h
@@ -0,0 +1,33 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define NOMINMAX
+
+#include <windows.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <io.h>
+
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdarg>
+
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <vector>
+
+// TODO: reference additional headers your program requires here
